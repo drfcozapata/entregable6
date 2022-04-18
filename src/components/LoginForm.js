b43bed64ff388b506bfd4d0b66ec75b1f3368c21
Francisco Zapata <drfcozapata@gmail.com>
2022-04-18 10:23:48 -0400

    Log out

---
+++ src/components/LoginForm.js
@@ -0,0 +1,96 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { ReactComponent as CircleUser } from '../assets/circle-user.svg';
+import { ReactComponent as Envelope } from '../assets/envelope.svg';
+import { ReactComponent as Key } from '../assets/key.svg';
+import '../styles/login-form.css';
+
+const LoginForm = ({ isLogin, setIsLogin }) => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [userName, setUserName] = useState('');
+
+	const handleLogin = e => {
+		e.preventDefault();
+		const userCredentials = {
+			email,
+			password,
+		};
+		axios
+			.post(
+				'https://ecommerce-api-react.herokuapp.com/api/v1/users/login/',
+				userCredentials
+			)
+			.then(res => {
+				const user = `${res.data?.data.user.firstName} ${res.data?.data.user.lastName}`;
+				setUserName(user);
+				// console.log(res.data);
+				// console.log(user);
+				localStorage.setItem('token', res.data?.data.token);
+			});
+
+		setEmail('');
+		setPassword('');
+		setIsLogin(false);
+	};
+
+	return (
+		<div className="navegation-login">
+			<form
+				onSubmit={handleLogin}
+				className={`login form-group card card-body ${isLogin ? 'open' : ''}`}
+			>
+				{localStorage.getItem('token') ? (
+					<>
+						<div className="login-image">
+							<CircleUser />
+						</div>
+						<h3 className="text-center mb-4">{userName}</h3>
+						<button onClick={() => localStorage.setItem('token', '')}>
+							Log out
+						</button>
+					</>
+				) : (
+					<>
+						<div className="login-image">
+							<CircleUser />
+						</div>
+						<div className="login-test">
+							<p className="text-center">
+								<span>Test Data</span>
+							</p>
+							<div className="d-flex align-items-center ml-10">
+								<Envelope />
+								<p>drfcozapata@gmail.com</p>
+							</div>
+							<div className="d-flex align-items-center ml-10">
+								<Key />
+								<p>pass1234</p>
+							</div>
+						</div>
+						<input
+							className="form-control"
+							type="email"
+							placeholder="email"
+							value={email}
+							onChange={e => setEmail(e.target.value)}
+						/>
+						<input
+							className="form-control"
+							type="password"
+							placeholder="password"
+							value={password}
+							onChange={e => setPassword(e.target.value)}
+						/>
+						<button className="btn btn-danger">Login</button>
+						<p className="text-center login-account">
+							Dont have an account? Sign Up
+						</p>
+					</>
+				)}
+			</form>
+		</div>
+	);
+};
+
+export default LoginForm;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,34 +3,11 @@ import '../styles/nav-bar.css';
 import { ReactComponent as User } from '../assets/user.svg';
 import { ReactComponent as Archive } from '../assets/archive.svg';
 import { ReactComponent as Cart } from '../assets/cart.svg';
-import { ReactComponent as CircleUser } from '../assets/circle-user.svg';
-import { ReactComponent as Envelope } from '../assets/envelope.svg';
-import { ReactComponent as Key } from '../assets/key.svg';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+import LoginForm from './LoginForm';
 
 const NavBar = () => {
 	const [isLogin, setIsLogin] = useState(false);
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-
-	const handleLogin = e => {
-		e.preventDefault();
-		const userCredentials = {
-			email,
-			password,
-		};
-		axios
-			.post(
-				'https://ecommerce-api-react.herokuapp.com/api/v1/users/login',
-				userCredentials
-			)
-			.then(res => localStorage.setItem('token', res.data?.data.token));
-
-		setEmail('');
-		setPassword('');
-		setIsLogin(false);
-	};
 
 	return (
 		<header className="header">
@@ -60,47 +37,7 @@ const NavBar = () => {
 				</ul>
 			</nav>
 
-			<div className="navegation-login">
-				<form
-					onSubmit={handleLogin}
-					className={`login form-group card card-body ${isLogin ? 'open' : ''}`}
-				>
-					<div className="login-image">
-						<CircleUser />
-					</div>
-					<div className="login-test">
-						<p className="text-center">
-							<span>Test Data</span>
-						</p>
-						<div className="d-flex align-items-center ml-10">
-							<Envelope />
-							<p>drfcozapata@gmail.com</p>
-						</div>
-						<div className="d-flex align-items-center ml-10">
-							<Key />
-							<p>pass1234</p>
-						</div>
-					</div>
-					<input
-						className="form-control"
-						type="email"
-						placeholder="email"
-						value={email}
-						onChange={e => setEmail(e.target.value)}
-					/>
-					<input
-						className="form-control"
-						type="password"
-						placeholder="password"
-						value={password}
-						onChange={e => setPassword(e.target.value)}
-					/>
-					<button>Login</button>
-					<p className="text-center login-account">
-						Dont have an account? Sign Up
-					</p>
-				</form>
-			</div>
+			<LoginForm isLogin={isLogin} setIsLogin={setIsLogin} />
 		</header>
 	);
 };
